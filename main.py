448a95c19be9ca66a2ced0164c7d2e8b8a8e37e3
yutaweb <yuta3984@gmail.com>
2021-09-13 23:47:22 +0900

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+N, C = map(int, input().split())
+event = []
+for i in range(N):
+    a, b, c = map(int, input().split())
+    a -= 1
+    event.append((a, c))
+    event.append((b, c))
+
+
+event.sort()
+ans = 0
+fee = 0
+t = 0
+for x, y in event:
+    if x == t:
+        fee += y
+    fee += y
+
+print(fee)
+
