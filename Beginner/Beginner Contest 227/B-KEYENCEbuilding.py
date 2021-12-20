ccb1fd66c67d37b4d5a4bf7ac84548c812226f62
yutaweb <yuta3984@gmail.com>
2021-12-21 00:59:19 +0900

    'B-KEYENCEbuilding.py追加'

--- Beginner/Beginner Contest 227/B-KEYENCEbuilding.py
+++ Beginner/Beginner Contest 227/B-KEYENCEbuilding.py
@@ -1,16 +1,32 @@
-# 4(a + 3/4)(b + 3/4) - 9/4
-# 10 = 4(a + 3/4)(b + 3/4) - 9/4
-# (10 + 9/4)/4 = (a + 3/4)(b + 3/4)
-N = int(input())
-L = list(map(int, input().split()))
-ans = 0
-for l in L:
-    flag=False
-    for a in range(1001):
-        for b in range(1001):
-            if 4*a*b+3*a+3*b == l:
-                flag=True
-    if not flag:
-        ans+=1
+import math
 
-print(ans)
+def main(data):
+    N = data[0]
+    S = data[1].split(" ")
+
+    count = 0
+    for val in S:
+        base = math.ceil((int(val) - 3)/7)
+        flag = False
+        # print(f"val : {val} base : {base}")
+        for a in range(1, base + 1):
+            # print(f"a : {a}")
+            if flag:
+                break
+            for b in range(1, base + 1):
+                # print(f"b : {b}")
+                if int(val) == 4*a*b+3*a+3*b:
+                    flag = True
+                    break
+
+        if not flag:
+            count += 1
+
+    print(count)
+
+if __name__ == "__main__":
+   data = []
+   with open('./input-B.txt') as f:
+       for line in f:
+           data.append(line.strip('\r\n'))
+   main(data)`
