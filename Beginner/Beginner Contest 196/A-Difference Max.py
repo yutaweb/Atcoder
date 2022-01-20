c1cb5ad2a96b5dd037f063527d6aee079d264081
yutaweb <yuta3984@gmail.com>
2022-01-20 10:53:55 +0900

    差分コミット

---
+++ Beginner/Beginner Contest 196/A-Difference Max.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+ans = b - c
+print(ans)
--- Beginner/Beginner Contest 196/C-Doubled.py
+++ Beginner/Beginner Contest 196/C-Doubled.py
@@ -1,18 +1,1 @@
-# N = input()
-# length = len(N)
-# before_num = N[0:int(length/2)]
-# after_num = N[int(length/2):length]
-# if int(after_num) == 0:
-#     ans = int(before_num) - 1
-# elif int(before_num) > int(after_num):
-#     ans = int(after_num)
-# else:
-#     ans = int(before_num)
-#
-# print(ans)
-
 N = int(input())
-for i in range(1, 1000001):
-    if int(str(i) * 2) > N:
-        print(i - 1)
-        exit()
