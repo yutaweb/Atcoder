3765d64a980a27505ccc3c9e709645bfcc4b6f35
Yuta Toyama <yuta3984@gmail.com>
2023-04-30 17:56:23 +0900

    修正

--- AGC/Grand Contest 013/A-Sorted-Arrays.py
+++ AGC/Grand Contest 013/A-Sorted-Arrays.py
@@ -4,18 +4,20 @@ A = list(map(int, input().split()))
 # 極大・極小を見つける
 ans = 0
 i = 0
-while i < N - 1:
+while i < N:
     j = i
-    print(j)
-    if A[j] <= A[j+1]:
+    while j + 1 < N and A[j] == A[j+1]:
+        j += 1
+
+    if j + 1 < N and A[j] < A[j+1]:
         # 単調増加
-        while j < N - 1 and A[j] <= A[j+1]:
+        while j + 1 < N and A[j] <= A[j+1]:
             j += 1
-    else:
+    elif j + 1 < N and A[j] > A[j+1]:
         # 単調減少
-        while j < N - 1 and A[j] >= A[j+1]:
+        while j + 1 < N and A[j] >= A[j+1]:
             j += 1
-    print(i, j+1, A[i:j+1])
+
     ans += 1
     i = j + 1
 
